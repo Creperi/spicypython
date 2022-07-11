7a091ea82ba7c4a34cd964293ae495ab8a9f91b0
Elias Poniridis <68995203+Creperi@users.noreply.github.com>
2022-07-11 08:13:32 +0300

    Add files via upload

---
+++ SpicyPython/addcomponents.py
@@ -0,0 +1,6 @@
+# This function writes the info of electrical component to a .sp file
+def file(filename):
+    def open():
+        if f == 0:
+            f = open(filename, "w")
+    def addsource(name, voltage:
